4c18b85b9af3ff0190d0cf7760743b5c427dbccd
codermine29 <nishthakapoor2017@gmail.com>
2021-08-26 23:22:06 +0530

    first commit

---
+++ src/editable-stuff/config.js
@@ -0,0 +1,158 @@
+// Navigation Bar SECTION
+const navBar = {
+  show: true,
+};
+
+// Main Body SECTION
+const mainBody = {
+  gradientColors: "#261C2C, #3E2C41, #5C527F, #6E85B2, #ff7f7f, #ecf0f1",
+  firstName: "Nishtha",
+  middleName: "",
+  lastName: "Kapoor",
+  message: " I'd suggest you to wait till the background changes to a light color..wait....now see the header. ",
+  icons: [
+    {
+      image: "fa-github",
+      url: "https://github.com/codermine29",
+    },
+    // {
+    //   image: "fa-facebook",
+    //   url: "https://www.facebook.com/hashirshoaeb",
+    // },
+    {
+      image: "fa-instagram",
+      url: "https://www.instagram.com/yea_thats_my_username",
+    },
+    {
+      image: "fa-linkedin",
+      url: "https://www.linkedin.com/in/nishtha-kapoor-63413b1a4/",
+    },
+  ],
+};
+
+// ABOUT SECTION
+// If you want the About Section to show a profile picture you can fill the profilePictureLink either with:
+//a) your Instagram username
+//      i.e:profilePictureLink:"johnDoe123",
+//b) a link to an hosted image
+//      i.e:profilePictureLink:"www.picturesonline.com/johnDoeFancyAvatar.jpg",
+//c) image in "editable-stuff" directory and use require("") to import here,
+//      i.e: profilePictureLink: require("../editable-stuff/hashirshoaeb.png"),
+//d) If you do not want any picture to be displayed, just leave it empty :)
+//      i.e: profilePictureLink: "",
+// For Resume either provide link to your resume or import from "editable-stuff" directory
+//     i.e resume: require("../editable-stuff/resume.pdf"),
+//         resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+
+const about = {
+  show: true, 
+  heading: "About Me",
+  //imageLink: //require("../editable-stuff/naqeeb.jpg"),
+  //imageSize: 375,
+  message:
+    "Hey, I'm Nishtha Kapoor. Currently in my final year of my undergraduate degree in Information Technology Engineering from Guru Gobind Singh Indraprastha University.I would describe myself as someone who is deeply fascinated by Technology and Innovation. I'm passionate about Web Dev and coding and my goal is to pursue this passion within the field of software development. ",
+  resume: "https://drive.google.com/file/d/1-IoBJOmSEYAjELrrDhcyKh1_siWZ7ob2/view?usp=sharing",
+};
+
+// PROJECTS SECTION
+// Setting up project lenght will automatically fetch your that number of recently updated projects, or you can set this field 0 to show none.
+//      i.e: reposLength: 0,
+// If you want to display specfic projects, add the repository names,
+//      i.e ["repository-1", "repo-2"]
+const repos = {
+  show: true,
+  heading: "Recent Projects",
+  gitHubUsername: "codermine29", //i.e."johnDoe12Gh"
+  reposLength: 4,
+  specificRepos: [""],
+};
+
+// Leadership SECTION
+const leadership = {
+  show: false,
+  heading: "Leadership",
+  message:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
+  images: [
+    { 
+     // img: require("../editable-stuff/naqeeb.jpg"), 
+      label: "First slide label", 
+      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+    },
+    { 
+      //img: require("../editable-stuff/naqeeb.jpg"), 
+      label: "Second slide label", 
+      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+    },
+  ],
+  imageSize: {
+    width:"450",
+    height:"450"
+  }
+};
+
+// SKILLS SECTION
+const skills = {
+  show: true,
+  heading: "Skills",
+  hardSkills: [
+    { name: "Bootstrap", value: 60 },
+    { name: "SQL", value: 75 },
+    { name: "Data Structures", value: 85 },
+    { name: "C/C++", value: 85 },
+    { name: "JavaScript", value: 80 },
+    { name: "React", value: 65 },
+    { name: "HTML/CSS", value: 55 },
+   // { name: "Arduino", value: 80 },
+    //{ name: "raspberry pi ", value: 50 },
+    { name: "Problem solving", value: 80 },
+    { name: "Gcloud", value: 70 },
+    { name: "Azure", value: 70 },
+  ],
+  softSkills: [
+    { name: "Goal-Oriented", value: 80 },
+    { name: "Collaboration", value: 90 },
+    { name: "Positivity", value: 75 },
+    { name: "Adaptability", value: 85 },
+    { name: "Problem Solving", value: 75 },
+    { name: "Empathy", value: 90 },
+    { name: "Organization", value: 70 },
+    { name: "Creativity", value: 90 },
+    { name: "Impactful", value: 90 },
+    { name: "Leadership", value: 90 },
+  ],
+};
+
+// GET IN TOUCH SECTION
+const getInTouch = {
+  show: true,
+  heading: "Get In Touch",
+  message:
+    "I'm currently looking for a full time role in Software Engineering! If you know of any positions available, or if you have any questions, or if you just want to say hi, please feel free to email me at",
+  email: "nishthakapoor2017@gmail.com",
+};
+
+const experiences = {
+  show: false,
+  heading: "Experiences",
+   data: [
+    {
+     role: 'Content Writer',
+   //   degree:'Electronic and Communication Engineering',
+   //   companylogo: require('../assets/img/dell.png'),
+     date: 'June 2018 – July 2018',
+    },
+    {
+      role: 'Graphic Design intern',
+      //companylogo: require('../assets/img/boeing.png'),
+      date: 'May 2020 – August 2020',
+    },
+  ]
+}
+
+// Blog SECTION
+// const blog = {
+//   show: false,
+// };
+
+export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
