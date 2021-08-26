f084bcd806e62fb0d8ed68aaa490ccc62002417c
Naqeeb Ahmed <naqeeb532@outlook.com>
2021-08-27 00:58:34 +0530

    Updates

---
+++ precache-manifest.e574ff092861c36104fc1837bb0b05f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e163761cd38456b010c8d1756d1d0be",
+    "url": "/fluffy-pancake/index.html"
+  },
+  {
+    "revision": "6a61d8d70086bb217cd9",
+    "url": "/fluffy-pancake/static/css/main.e16032d7.chunk.css"
+  },
+  {
+    "revision": "fc5d60bd4b4fab7d97a4",
+    "url": "/fluffy-pancake/static/js/2.734e6852.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/fluffy-pancake/static/js/2.734e6852.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a61d8d70086bb217cd9",
+    "url": "/fluffy-pancake/static/js/main.a13a65a7.chunk.js"
+  },
+  {
+    "revision": "8b13022267ccdf3fe5fa",
+    "url": "/fluffy-pancake/static/js/runtime-main.c3666c8e.js"
+  },
+  {
+    "revision": "665544b99cb88eb974fcc7c52d767a13",
+    "url": "/fluffy-pancake/static/media/resume.665544b9.pdf"
+  }
+]);
